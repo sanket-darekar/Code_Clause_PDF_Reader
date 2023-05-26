827224b5d1fda19762d59eaa0ba31d91ecf589b0
sanket-darekar <113106195+sanket-darekar@users.noreply.github.com>
2023-05-26 17:07:10 +0530

    Add files via upload

---
+++ src/main/java/com/example/pdfreader/adapter_pdf.java
@@ -0,0 +1,14 @@
+package com.example.pdfreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class adapter_pdf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adapter_pdf);
+    }
+}
